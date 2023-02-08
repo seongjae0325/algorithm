8fb6fa6c14f84d00386b17b2c6f9660b08fa9320
wong0220 <dlsdnd0220@naver.com>
2023-02-08 23:58:29 +0900

    게임 맵 최단 거리, 타겟넘버 문제풀이

--- DFS/BFS/woong/게임맵최단거리.js
+++ DFS/BFS/woong/게임맵최단거리.js
@@ -27,7 +27,6 @@
 //   while(needVisited.length !==0){
 //     const node = needVisited.shift()
 
-
 //     if(!visited.includes(node)){
 //       visited.push(node)
 //       if(visitedNode[node] === unedifined) visitedNode[node] = []
@@ -42,45 +41,46 @@
 
 //case2
 function solution(maps) {
-  const dx = [-1,1,0,0]
-  const dy = [0,0,-1,1]
-  const queue = []
-  const m = maps.length -1
-  const n = maps[0].length -1
-  let copiedMatrix = maps
-  let counter = 1
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  const queue = [];
+  const m = maps.length - 1;
+  const n = maps[0].length - 1;
+  let copiedMatrix = maps;
+  let counter = 1;
 
-  copiedMatrix[0][0] = 0
-  queue.push([0,0])
+  copiedMatrix[0][0] = 0;
+  queue.push([0, 0]);
 
-  while(queue.length !== 0){
+  while (queue.length !== 0) {
+    const size = queue.length;
 
-    const size = queue.length
+    for (let i = 0; i < size; i++) {
+      const [x, y] = queue.shift();
 
+      for (let j = 0; j < 4; j++) {
+        const nx = x + dx[j];
+        const ny = y + dy[j];
 
-    for(let i = 0 ; i < size ; i++){
-      const [x,y] = queue.shift()
-
-      for(let j = 0 ; j < 4; j++){
-        const nx = x + dx[j]
-        const ny = y + dy[j]
-
-        if(nx > -1 && ny > -1 && nx < m + 1 && ny < n + 1 &&  copiedMatrix[nx][ny] === 1){
-          if(nx === m && ny === n){
-            return ++counter
+        if (
+          nx > -1 &&
+          ny > -1 &&
+          nx < m + 1 &&
+          ny < n + 1 &&
+          copiedMatrix[nx][ny] === 1
+        ) {
+          if (nx === m && ny === n) {
+            return ++counter;
           }
 
-
-          queue.push([nx,ny])
-          copiedMatrix[nx][ny] = 0
+          queue.push([nx, ny]);
+          copiedMatrix[nx][ny] = 0;
         }
-
       }
     }
 
-    counter++
+    counter++;
   }
 
-
-  return -1
+  return -1;
 }
